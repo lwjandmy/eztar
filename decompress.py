d9407cbf57911e4d8aa6b21f3829a51a8a8f3d89
lwjandmy <lwjandmy@qq.com>
2016-01-23 13:25:58 +0800

    add {OUT} in decompress

--- decompress.py
+++ decompress.py
@@ -5,7 +5,7 @@ from plugin_manager import load_plugins_from_suffix
 
 def decompress_file(file_name, suffix_name):
 	plugin = load_plugins_from_suffix(suffix_name)
-	decompress_cmd = plugin['decompress_command'].format(IN = file_name)
+	decompress_cmd = plugin['decompress_command'].format(IN = file_name, OUT = file_name[:-len(suffix_name)-1])
 	print('decompress execute: ' + decompress_cmd)
 	os.system(decompress_cmd)
 
